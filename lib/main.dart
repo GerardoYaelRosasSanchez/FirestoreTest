e0acc9920ddccf608c19d2949aab34d22f6803dc
Gerardo Yael Rosas Sanchez <courtlycartoon33@gmail.com>
2022-08-12 21:49:14 -0500

    Future Firebase implementation. Firebase instance is created2

--- lib/main.dart
+++ lib/main.dart
@@ -10,27 +10,64 @@ void main() async {
     // Solves the error.
   WidgetsFlutterBinding.ensureInitialized();
     // Await le dice a Flutter que no inicie la aplicación hasta que inicie Firebase.
-  await Firebase.initializeApp(); 
+      //  This is used if future is not used.
+  //await Firebase.initializeApp(); 
   runApp(MyApp());
 }
 
-//void main() => runApp(const MyApp());
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+  // Future representa un posible valor que será disponible en el futuro.
+    // En este caso vera si se logro recuperar la instancia de Firebase. 
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp(); 
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      /*
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        title: const Text('Material App Bar'),
       ),
+      body: const Center(
+        child: Text('Conexión exitosa.'),
+      ),
+      ),
+      */ 
+
+      // FutureBuilder Ayuda a determinar el estado del Future y que mostrar si aún esta cargando. 
+      home: FutureBuilder(
+        future: _fbApp, // Que futuro voy a estar monitoreando
+        builder: (context, snapshot) { // Se llama al inicio o cuando hay un cambio en el estado del Futuro.
+          // Revisa si hubo un error al recuperar la instancia de Firebase.
+          if (snapshot.hasError){
+            print('Hubo un error. ${snapshot.error.toString()}');
+            return Text('Error al inicializar Firebase.');
+          }
+          // Revisa si la instancia de Firebase se recupero exitosamente.  
+          else if (snapshot.hasData){
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Material App Bar'),
+              ),
+              body: const Center(
+                child: Text('Conexión exitosa.'),
+              ),
+            ); 
+          }
+          // Si aún esta buscando, muestra un indicador de espera. 
+          else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },   
+      ),
+      
+
+      
+      
     );
   }
 }

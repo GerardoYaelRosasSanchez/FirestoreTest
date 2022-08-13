f0bb251059c3f0935a168f09042709ac73e2f356
Gerardo Yael Rosas Sanchez <courtlycartoon33@gmail.com>
2022-08-12 20:58:54 -0500

    Initialize Firebase Library

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(const MyApp());
+
+void main() async {
+  // Initialize Firebase Library
+    // Allows to proccess session start events.
+    // Sign User in.
+  // Needed to access binary messeger before runApp() has been called.
+    // Solves the error.
+  WidgetsFlutterBinding.ensureInitialized();
+    // Await le dice a Flutter que no inicie la aplicación hasta que inicie Firebase.
+  await Firebase.initializeApp(); 
+  runApp(MyApp());
+}
+
+//void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
